c2b351c55745c41180b46cf549569a12e66de9e5
newki707-star <newki707@gmail.com>
2026-08-15 03:15:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const IMAGES = {
-  attic: "./images/attic.png",
-  beach: "./images/beach.png",
-  boat: "./images/boat.png",
-  cave: "./images/cave.png",
-  desk: "./images/desk.png",
-  fog: "./images/fog.png",
-  hall_clock: "./images/hall_clock.png",
-  house: "./images/house.png",
-  lighthouse: "./images/lighthouse.png",
-  shadow: "./images/shadow.png",
-  stars: "./images/stars.png"
+  attic: "images/attic.png",
+  beach: "images/beach.png",
+  boat: "images/boat.png",
+  cave: "images/cave.png",
+  desk: "images/desk.png",
+  fog: "images/fog.png",
+  hall_clock: "images/hall_clock.png",
+  house: "images/house.png",
+  lighthouse: "images/lighthouse.png",
+  shadow: "images/shadow.png",
+  stars: "images/stars.png"
 };
 
 const storyData = {
